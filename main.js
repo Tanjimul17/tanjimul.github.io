1b0f166cf149ac2234420a92c22cad07e6ccd147
Tanjimul17 <68654298+Tanjimul17@users.noreply.github.com>
2021-01-07 13:00:38 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+var x = '3';
+var y = '4';
+var u = '+';
+var t = eval(x+u+y);
+console.log (t);
+function cl(x)
+    {console.log(x);}
+    ;
+
+function plus (x, y){
+   var p='+';
+   var pluse =eval (x+p+y);
+   console.log(pluse);
+}
+plus('20','70');
+plus ('1','66',"88");
+var c = window.innerWidth;
+cl(c);
